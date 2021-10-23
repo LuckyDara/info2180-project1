c78e05b093028e173f22f20f4fbffd2cf228ccd9
LuckyDara <dandangoulbourne@gmail.com>
2021-10-23 02:17:56 -0500

    Final submission: CHanges made to necessary files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
 /* Add your JavaScript to this file */
+
+function submitEmail(){
+    if(document.getElementById("email").value==""){
+        document.getElementById("email").value="Please enter valid email";
+    }else{
+        document.getElementById("email").value= "Thank you! Your email address "+document.getElementById("email").value+" has been added to our mailing list."
+    }
+}
